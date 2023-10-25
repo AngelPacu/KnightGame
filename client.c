da4001d0abd876725cdf9368fcd6371c371e60d6
Angel Parra <92107694+AngelPacu@users.noreply.github.com>
2023-10-26 00:31:22 +0200

    Update: add a random number function and conex.

--- client.c
+++ client.c
@@ -14,47 +14,94 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
+/* Inclusio de time.h per calcular el numero aleatori */
+#include <time.h>
+
 #define MIDA_BUFFER 1024
 
-int main(int argc, char **argv)
+struct DataClient 
 {
+    uint32_t id;
+    char command;
 
-    if (argc == 3)
-    {
+};
 
-        int s; /* Per treballar amb el socket */
-        struct sockaddr_in adr;
-        char buffer[MIDA_BUFFER];
-        int i;
 
-        /* Volem socket d'internet i no orientat a la connexio */
-        s = socket(AF_INET, SOCK_DGRAM, 0);
+uint32_t generarIdAleatoria() {
+    // Inicializa la semilla del generador de números aleatorios con la hora actual
+    srand(time(NULL));
 
-        adr.sin_family = AF_INET;
-        adr.sin_port = htons(atoi(argv[2]));
+    // Genera 4 números de 8 bits y los combina en un número de 32 bits
+    uint32_t randomNum = 0;
+    for (int i = 0; i < 4; i++) {
+        uint32_t aleatorio8Bits = rand() & 0xFF; // Genera un número aleatorio de 8 bits
+        randomNum = (randomNum << 8) | aleatorio8Bits; // Combina los bits
+    }
 
-        /* L'adreca de comunicacio sera la IP del servidor, es a dir el parametre */
-        adr.sin_addr.s_addr = inet_addr(argv[1]);
+    return randomNum;
+}
 
-        /* Generem 5 paquets i els enviem */
-        for (i = 0; i < 5; i++)
-        {
-            buffer[0] = i + '0';
-            buffer[1] = '\0';
+int main(int argc, char *argv[]) {
+    char response;
+    struct sockaddr_in server_addr;
+    struct DataClient package;
 
-            sendto(s, buffer, MIDA_BUFFER, 0, (struct sockaddr *)&adr, sizeof(adr));
+    package.id = generarIdAleatoria();
+    printf("Número aleatorio de 32 bits: %u\n", package.id);
 
-            printf("Paquet enviat!\n");
+    // Crear socket del cliente
+    int client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_socket == -1) {
+        perror("Error al crear el socket del cliente");
+        exit(2);
+    }
+
+    // Definir la dirección del servidor
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(atoi(argv[2]));
+    inet_pton(AF_INET, argv[1], &server_addr.sin_addr);
+
+    // Conectar al servidor
+    if (connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+        perror("Error al conectar al servidor");
+        exit(3);
+    }
+
+        // Implementa la lógica del juego y la comunicación con el servidor aquí
+        // Envía comandos al servidor y recibe respuestas
+
+        // Leer el comando del usuario (U, D, L, R)
+        printf("Ingrese un comando (U, D, L, R): ");
+        scanf(" %c", &package.command);
+
+        // Enviar el comando al servidor
+        send(client_socket, &package, sizeof(struct DataClient), 0);
+
+        // Recibir la respuesta del servidor
+        recv(client_socket, &response, sizeof(char), 0);
+
+        // Procesar la respuesta
+        switch (response) {
+            case ' ': // Casilla libre
+                printf("Casilla libre. Continúa.\n");
+                break;
+            case 'T': // Casilla con trampa
+                printf("¡Has caído en una trampa! Pierdes una vida.\n");
+                break;
+            case 'P': // Casilla de pared
+                printf("No puedes pasar. Elige otro camino.\n");
+                break;
+            case 'V': // Casilla que incrementa una vida
+                printf("¡Has encontrado una vida extra!\n");
+                break;
+            case 'S': // Puerto de llegada
+                printf("DATAGRAMA ENTREGADO!! Has ganado el juego.\n");
+                close(client_socket); // Cerrar el socket
+                exit(0);
         }
 
-        /* Tanquem el socket */
-        close(s);
-    }
 
-    else
-    {
-        printf("El nombre de paràmetres no és el correcte!\n");
+        // Cierra el socket del cliente
+        close(client_socket);
+        return 0;
     }
-
-    return 0;
-}
--- server.c
+++ server.c
@@ -1,66 +1,119 @@
-#include <stdlib.h> 
-#include <string.h> 
- 
-/* Inclusio de fitxers .h per als sockets */ 
-#include <sys/socket.h> 
-#include <sys/types.h> 
-#include <netinet/in.h> 
-#include <arpa/inet.h> 
-#include <unistd.h> 
- 
-#define MIDA_BUFFER 1024 
- 
-int main(int argc, char **argv) 
-{ 
- 
-    if (argc == 2) 
-    { 
-        int s; /* Per treballar amb el socket */ 
-        struct sockaddr_in serv_adr, client_adr; 
-        char buffer[MIDA_BUFFER]; 
-        socklen_t mida; 
-        int n, i; 
-        int port; 
- 
-        /* Volem socket d'internet i no orientat a la connexio */ 
-        s = socket(AF_INET, SOCK_DGRAM, 0); 
- 
-        /* Posem les dades del socket */ 
-        serv_adr.sin_family = AF_INET; 
-        serv_adr.sin_addr.s_addr = INADDR_ANY; 
-        serv_adr.sin_port = htons(atoi(argv[1])); 
- 
-        /* Enllacem el socket */ 
-        n = bind(s, (struct sockaddr *)&serv_adr, sizeof(serv_adr)); 
- 
-        if (n < 0) 
-        { 
-            printf("No s'ha pogut enllacar el socket\n"); 
-        } 
-        else 
-        { 
-            i = 0; 
-            /* Servidor operatiu! */ 
-            printf("Servidor operatiu al port %d!\n", atoi(argv[1])); 
- 
-            while (i < 5) /* Nomes rebre cinc paquets */ 
-            { 
-                recvfrom(s, buffer, MIDA_BUFFER, 0, (struct sockaddr *)&client_adr, &mida); 
-                printf("Paquet rebut!\n"); 
-                printf("%s\n", buffer); 
-                i++; 
-            } 
-        } 
- 
-        /* Tanquem el socket */ 
-        close(s); 
-    } 
- 
-    else 
-    { 
-        printf("El nombre de paràmetres no és el correcte!\n"); 
-    } 
-    return 0; 
-} 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
-  
+// Tamaño del tablero
+#define ROWS 5
+#define COLS 5
+
+struct DataClient 
+{
+    uint32_t id;
+    char command;
+
+};
+
+// Definir el tablero inicial
+char board[ROWS][COLS] = {
+    {' ', ' ', ' ', 'P', 'S'},
+    {'T', 'V', ' ', ' ', 'P'},
+    {' ', 'T', ' ', 'T', ' '},
+    {'P', ' ', 'T', ' ', 'V'},
+    {' ', ' ', ' ', 'T', 'T'}
+};
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Uso: %s <Puerto del servidor>\n", argv[1]);
+        exit(1);
+    }
+    
+
+    // Crear socket del servidor
+    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket == -1) {
+        perror("Error al crear el socket del servidor");
+        exit(2);
+    }
+
+    // Configurar la dirección del servidor
+    struct sockaddr_in server_addr;
+        server_addr.sin_family = AF_INET; 
+        server_addr.sin_addr.s_addr = INADDR_ANY; 
+        server_addr.sin_port = htons(atoi(argv[1])); 
+    // Enlazar el socket a la dirección
+    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+        perror("Error al enlazar el puerto");
+        exit(3);
+    }
+
+    // Escuchar en el puerto
+    if (listen(server_socket, 5) == -1) {
+        perror("Error al escuchar en el puerto");
+        exit(4);
+    }
+
+    // *** Servidor Operativo ***
+    fprintf(stderr, "Servidor operativo por el puerto %s\n", argv[1]);
+    while (1) {
+        // Aceptar conexiones de clientes
+        int client_socket = accept(server_socket, NULL, NULL);
+        if (client_socket == -1) {
+            perror("Error al aceptar la conexión del cliente");
+            continue;
+        }
+
+        // Lógica del juego y comunicación con el cliente
+        int lives = 3;
+        int row = 0;
+        int col = 0;
+
+        while (lives > 0) {
+            // Recibir el comando del cliente
+            struct DataClient packClient;
+            recv(client_socket, &packClient, sizeof(struct DataClient), 0);
+            printf("EL id del cliente %u y su respuesta: %c \n ",packClient.id,packClient.command);
+            printf("prueba fila %d prueba col %d\n",row, col);
+            // Procesar el comando y actualizar la posición del caballero
+            switch (packClient.command) {
+                case 'U':
+                    if (row > 0) row--;
+                    break;
+                case 'D':
+                    if (row < ROWS - 1) row++;
+                    break;
+                case 'L':
+                    if (col > 0) col--;
+                    break;
+                case 'R':
+                    if (col < COLS - 1) col++;
+                    break;
+            }
+            printf("prueba fila %d prueba col %d\n",row, col);
+            // Actualizar las vidas según la casilla actual
+            if (board[row][col] == 'T') {
+                lives--;
+            } else if (board[row][col] == 'V') {
+                lives++;
+            } else if (board[row][col] == 'S') {
+                // El cliente ganó el juego
+                char win_message = 'S';
+                send(client_socket, &win_message, sizeof(char), 0);
+                break;
+            }
+            // Enviar el estado actual del tablero al cliente
+            char current_cell = board[row][col];
+            send(client_socket, &current_cell, sizeof(char), 0);
+        }
+
+        // Cerrar el socket del cliente
+        close(client_socket);
+    }
+
+    // Cerrar el socket del servidor
+    close(server_socket);
+
+    return 0;
+}
